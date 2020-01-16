47ae21abc9f3639fc1e5b882360a6136130e70b4
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2020-01-15 23:24:58 -0500

    project started

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const db = require("./data/db");
+const express = require("express");
+const server = express();
+server.listen(4000, () => {
+  console.log("holla if you hear me");
+});
+server.use(express.json());
